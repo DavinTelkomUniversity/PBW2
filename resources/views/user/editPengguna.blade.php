cdc5e9aa732d5b2c70fe87c710c3dfb84728857d
Davin Wardana <36639401+davinwardana@users.noreply.github.com>
2023-10-22 00:09:49 +0700

    slot
    Signed-off-by: Davin Wardana <36639401+davinwardana@users.noreply.github.com>

--- resources/views/user/editPengguna.blade.php
+++ resources/views/user/editPengguna.blade.php
@@ -5,7 +5,6 @@
         </h2>
     </x-slot>
 
-    @section('content')
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -60,5 +59,4 @@
                 </div>
             </div>
         </div>
-    @endsection
 </x-app-layout>
