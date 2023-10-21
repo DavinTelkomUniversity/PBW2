9125911e9ce86b47598370788dbd9bd359234aad
Davin Wardana <36639401+davinwardana@users.noreply.github.com>
2023-10-22 00:04:58 +0700

    slot
    Signed-off-by: Davin Wardana <36639401+davinwardana@users.noreply.github.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,6 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -14,5 +13,4 @@
             </div>
         </div>
     </div>
-@endsection
 </x-app-layout>
